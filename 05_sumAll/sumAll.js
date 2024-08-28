898693c73fd85ed73630d2dc7b53d5e5000294e3
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-28 13:40:22 -0700

    Complete exercise 05
    Used an if statement to check for errors, then determined upper and
    lower limit. Finally, used a for loop to sum numbers in the range.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    if (num1 < 0 || num2 < 0 || typeof (num1) != "number" || typeof (num2) != "number" || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    let upperLimit = null;
+    let lowerLimit = null;
+    if (num1 > num2) {
+        upperLimit = num1;
+        lowerLimit = num2;
+    } else if (num2 > num1) {
+        upperLimit = num2;
+        lowerLimit = num1;
+    } else {
+        //same number.. just return either of them.
+        return num1;
+    }
+    let sum = 0;
+    for (currentNumber = lowerLimit; currentNumber <= upperLimit; ++currentNumber) {
+        sum += currentNumber;
+    }
+    return sum;
 };
 
 // Do not edit below this line
