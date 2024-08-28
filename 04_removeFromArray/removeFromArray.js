fc0d3b2172239b7a803c79b86b8d2778830f77c7
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-28 13:19:57 -0700

    Complete exercise 03
    Implemented using a nested for loop to cycle through arguments array and
    array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
+    // For each argument (after array), check and remove it from array.
+    for (i = 1; i < arguments.length; ++i) {
+        for (j = 0; j < array.length; ++j) {
+            if (array[j] === arguments[i]) {
+                // if current item in array is equal to current argument, remove that item.
+                array.splice(j, 1);
+                // Decrement j so we dont miss the item that just shifted into the place of the removed item.
+                --j;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
