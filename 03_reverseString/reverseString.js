6c5fa143ff351db9b846f959868978fc6f9ea9b3
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-28 12:54:36 -0700

    Complete exercise 03
    Implemented the function using recursion for practice.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+// Choosing to do a recursive implementation just to practice recursion...
+const reverseString = function (string) {
+    let reversedSubstr = "";
+    // If string is more than one character, reverse the string excluding the last character.
+    if (string.length > 1) {
+        reversedSubstr = reverseString(string.substring(0, string.length - 1));
+    }
+    // The reversed string will be the last letter concatenated with the reversed substring. i.e: CAT is T + AC (which is CA reversed)
+    // If string is only 1 letter, this will return only that letter since reversedSubStr is initialized to empty string.
+    return string.charAt(string.length - 1) + reversedSubstr;
 };
 
 // Do not edit below this line
