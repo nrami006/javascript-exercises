1e75292d5578ffe49ac3eec2a790bbba5c96fe6a
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-29 10:34:02 -0700

    Complete exercise 06
    Just took the logic of what makes a leap years and implemented it using
    nested if/else statements.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                // leap year
+                return true;
+            } else {
+                // not a leap year
+                return false;
+            }
+        } else {
+            // leap year
+            return true;
+        }
+    } else {
+        // not a leap year
+        return false;
+    }
 };
 
 // Do not edit below this line
