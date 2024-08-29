173c80f7e685392069e282dfe49a467da73be90b
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-29 12:12:31 -0700

    Complete exercise 10
    Implemented using a for loop.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    if (n <= 2) return 1;
+    let currentNum = 1;
+    let prevNum = 1;
+    let temp = null;
+    for (i = 2; i < n; ++i) {
+        temp = currentNum;
+        currentNum += prevNum
+        prevNum = temp;
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
