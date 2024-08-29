c3bbe10c70b0f0c8a8653e720389a7459bfabcb3
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-29 11:26:42 -0700

    Complete exercise 07

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return (x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return (x - y);
 };
 
-const sum = function() {
-	
+const sum = function (arrayOfNumbers) {
+  let sum = 0;
+  for (number of arrayOfNumbers) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arrayOfNumbers) {
+  let total = 1;
+  for (number of arrayOfNumbers) {
+    total *= number;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (x, n) {
+  return (x ** n);
 };
 
-const factorial = function() {
-	
+// x * (x-1) * x(-2)... * 1
+const factorial = function (x) {
+  // 1! and 0! are both 1.
+  if (x == 1 || x == 0) {
+    return 1;
+  }
+  // recursively calculate factorial. Its just cleaner. Might be better to just do a for loop for performance.
+  return x * factorial(x - 1);
 };
 
 // Do not edit below this line
