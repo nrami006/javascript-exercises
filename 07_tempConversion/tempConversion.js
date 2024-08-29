bee6fa642f5c9f79c4d8197f13ee0bceb01f84fc
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-29 10:41:59 -0700

    Complete exercise 07
    Just plugged in the formula from wikipedia for each function and
    rounded the results to the nearest tenth using Math.round() and some
    decimal moving.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemp) {
+  let celsiusTemp = (fahrenheitTemp - 32) * (5 / 9);
+  var rounded = Math.round(celsiusTemp * 10) / 10
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusTemp) {
+  let fahrenheitTemp = (celsiusTemp * (9 / 5) + 32);
+  let rounded = Math.round(fahrenheitTemp * 10) / 10
+  return rounded;
 };
 
 // Do not edit below this line
