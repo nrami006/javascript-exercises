c6aceefe315bd2acf84f5cd290bf90b30e2d57ef
Nat <70920883+nrami006@users.noreply.github.com>
2024-08-29 11:47:55 -0700

    Complete exercise 09
    Used a regular expression to remove punctiation and whitespace.
    Converted string to lowercase, then tested for palindrom using a for
    loop.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    //Strip string of all punctuation and white space
+    let strippedString = string.replace(/\W|_/g, '');
+    //Lowercase the string so capitalization doesnt matter. 
+    strippedString = strippedString.toLowerCase();
+    let headIndex = 0;
+    let tailIndex = strippedString.length - 1;
+    // Check if palindrome
+    while (headIndex <= tailIndex) {
+        if (strippedString[headIndex] != strippedString[tailIndex]) {
+            // Not a palindrome
+            return false;
+        }
+        ++headIndex;
+        --tailIndex;
+    }
+    //Made it out of the loop -- string is a palindrome.
+    return true;
 };
 
 // Do not edit below this line
